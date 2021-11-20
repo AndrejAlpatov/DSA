3649503e8f26892c2b0a0bd616c91a0fce5c4c51
Danny Staus <inf3719@hs-worms.de>
2021-11-20 13:27:08 +0100

    added the class StudierendenwerkInfoIntentHandler

---
+++ src/studierendenwerk_handler/studierendenwerk_info_intent_handler.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from pymongo import MongoClient
+from ask_sdk_core.skill_builder import SkillBuilder
+from flask_ask_sdk.skill_adapter import SkillAdapter
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model import Response
+
+class StudierendenwerkInfoIntentHandler(AbstractRequestHandler):
+    """Handler for StudierendenwerkInfoIntent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("StudierendenwerkInfoIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        speech_text = ""
+        reprompt = ""
+
+        handler_input.response_builder.speak(speech_text).ask(reprompt)
+        return handler_input.response_builder.response
