95bb21ee4e8606b64113c80dc2c628aa4f450d4e
Danny Staus <inf3719@hs-worms.de>
2021-11-20 11:02:37 +0100

    added the OperatorOfMensaHander implementation parts

--- src/studierendenwerk_handler/operator_of_mensa_handler.py
+++ src/studierendenwerk_handler/operator_of_mensa_handler.py
@@ -8,6 +8,7 @@ from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
+from random import randint
 
 class OperatorOfMensaIntentHandler(AbstractRequestHandler):
     """Handler for OperatorOfMensaIntent."""
@@ -19,6 +20,14 @@ class OperatorOfMensaIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
 
+        speech_text_list = ["Die Mensa wird von dem Studierendenwerk Vorderpfalz betrieben.",
+                            "Die Mensa wird von dem Studierendenwerk Vorderpfalz geleitet.",
+                            "Das Studierendenwerk Vorderpfalz ist für die Mensa verantwortlich.",
+                            "Die Mensa gehört dem Studierendenwerk Vorderpfalz."]
 
-        handler_input.response_builder.speak(speech_text)
+        list_index = randint(0, len(speech_text_list) - 1)
+
+        speech_text = speech_text_list[list_index]
+
+        handler_input.response_builder.speak(speech_text).ask(speech_text)
         return handler_input.response_builder.response
