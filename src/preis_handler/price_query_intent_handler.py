0c9d28809e28faade5251d9ac2b8da949d88f43e
Danny Staus <inf3719@hs-worms.de>
2021-11-27 21:40:29 +0100

    added the implementation and outsourced the answers to the DB

--- src/preis_handler/price_query_intent_handler.py
+++ src/preis_handler/price_query_intent_handler.py
@@ -14,11 +14,11 @@ from src.data_bank_functions.output_of_all_collections import data_bank_access
 class PriceQueryIntentHandler(AbstractRequestHandler):
     """Handler for PriceQueryIntent."""
 
-    def activate_session(self, db_collection_answers):
-        db_collection_answers.update_one({"session_active": False}, {"$set": {"session_active": True}})
+    def activate_session(self, db_collection_session):
+        db_collection_session.update_one({"SESSION_ACTIVE": False}, {"$set": {"SESSION_ACTIVE": True}})
 
-    def deactivate_session(self, db_collection_answers):
-        db_collection_answers.update_one({"session_active": True}, {"$set": {"session_active": False}})
+    def deactivate_session(self, db_collection_session):
+        db_collection_session.update_one({"SESSION_ACTIVE": True}, {"$set": {"SESSION_ACTIVE": False}})
 
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
@@ -26,64 +26,58 @@ class PriceQueryIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        """
+
         # Get DB collections
-        list_with_collections = data_bank_access(['answers_stud_werk'])
-        db_collection_answers = list_with_collections[0]
+        list_with_collections = data_bank_access(['sessions_price', 'answers_price'])
+        db_collection_session = list_with_collections[0]
+        db_collection_answers = list_with_collections[1]
+
+        # deactivate session if existing
+        self.deactivate_session(db_collection_session)
 
         # Get documents from collections
         answers = db_collection_answers.find_one({})
-
-        # List of all answers
-        speech_text_list = answers['OPERATOR_OF_MENSA']
-
-        # Select random answer
-        list_index = randint(0, len(speech_text_list) - 1)
-        speech_text = speech_text_list[list_index]
-
-        # Select reprompt
-        reprompt = answers['OPERATOR_OF_MENSA_REPROMPT']
-        """
-
-        # Get DB collections
-        list_with_collections = data_bank_access(['sessions_price'])
-        db_collection_answers = list_with_collections[0]
-
-        # deactivate session if existing
-        self.deactivate_session(db_collection_answers)
 
         # Get slots values
         slots = handler_input.request_envelope.request.intent.slots
         day = slots['day_price'].value
         membership = slots['membership_price'].value
         timeindication = slots['timeindication_price'].value
+        num_days = slots['num_days'].value
 
         # No slot given -> activate the price query session
-        if day is None and membership is None and timeindication is None:
-            speech_text = "Es gibt verschiedene Preise. Bist du Student oder Gast?"
-            self.activate_session(db_collection_answers)
+        if day is None and membership is None and timeindication is None and num_days is None:
+            # Select answer
+            speech_text = answers['STD_ANSWER']
+            # activate session
+            self.activate_session(db_collection_session)
+
+        # if any value for num_days is given
+        elif num_days is not None:
+            speech_text = answers['NUM_DAYS_ANSWER']
+            self.activate_session(db_collection_session)
 
         # day_price between montag and freitag -> price everyday the same and activate the price query session
         elif day == "montag" or day == "dienstag" or day == "mittwoch" or day == "donnerstag" or day == "freitag":
-            speech_text = "Die Preise für das Essen sind jeden Tag gleich. Bist du Student oder Gast?"
-            self.activate_session(db_collection_answers)
+            speech_text = answers['NUM_DAYS_ANSWER']
+            self.activate_session(db_collection_session)
 
         # day_price samstag or sonntag -> no food at weekend
         elif day == "samstag" or day == "sonntag" or day == "wochenende":
-            speech_text = "Am Wochenende wird kein Essen ausgegeben."
+            speech_text = answers['WEEKEND_ANSWER']
 
         # membership_price student or studenten or studentin or studentinnen -> 3 euro
         elif membership == "student" or membership == "studenten" or membership == "studentin" or membership == "studentinnen":
-            speech_text = "Für Studenten und Studentinnen kostet das Essen insgesamt drei Euro."
+            speech_text = answers['STUDENT_ANSWER']
 
         # membership_price gast or gäste -> 5 euro
         elif membership == "gast" or membership == "gäste":
-            speech_text = "Für Gäste kostet das Essen insgesamt fünf Euro."
+            speech_text = answers['GAST_ANSWER']
 
         # any timeindication -> activate the price query session
         elif timeindication is not None:
-            speech_text = "Die Preise für das Essen sind jeden Tag gleich. Bist du Student oder Gast?"
-            self.activate_session(db_collection_answers)
+            speech_text = answers['TIMEINDIC_ANSWER']
+            self.activate_session(db_collection_session)
 
         # something went wrong
         else:
--- src/preis_handler/price_query_session_intent_handler.py
+++ src/preis_handler/price_query_session_intent_handler.py
@@ -14,8 +14,8 @@ from src.data_bank_functions.output_of_all_collections import data_bank_access
 class PriceQuerySessionIntentHandler(AbstractRequestHandler):
     """Handler for PriceQuerySessionIntent."""
 
-    def deactivate_session(self, db_collection_answers):
-        db_collection_answers.update_one({"session_active": True}, {"$set": {"session_active": False}})
+    def deactivate_session(self, db_collection_session):
+        db_collection_session.update_one({"SESSION_ACTIVE": True}, {"$set": {"SESSION_ACTIVE": False}})
 
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
@@ -25,14 +25,16 @@ class PriceQuerySessionIntentHandler(AbstractRequestHandler):
         # type: (HandlerInput) -> Response
 
         # Get DB collections
-        list_with_collections = data_bank_access(['sessions_price'])
-        db_collection_answers = list_with_collections[0]
+        list_with_collections = data_bank_access(['sessions_price', 'answers_price'])
+        db_collection_session = list_with_collections[0]
+        db_collection_answers = list_with_collections[1]
 
         # Get documents from collections
+        session_answers = db_collection_session.find_one({})
         answers = db_collection_answers.find_one({})
 
         # Get status of session
-        session_status = answers['session_active']
+        session_status = session_answers['SESSION_ACTIVE']
 
         # check if the session is active
         if session_status == False:
@@ -46,18 +48,19 @@ class PriceQuerySessionIntentHandler(AbstractRequestHandler):
 
         # membership student or studenten or studentin or studentinnen -> 3 euro
         if membership == "student" or membership == "studenten" or membership == "studentin" or membership == "studentinnen":
-            speech_text = "Für Studenten und Studentinnen kostet das Essen insgesamt drei Euro."
+            # select answer
+            speech_text = answers['STUDENT_ANSWER']
 
         #membership gast or gäste -> 5 euro
         elif membership == "gast" or membership == "gäste":
-            speech_text = "Für Gäste kostet das Essen insgesamt fünf Euro."
+            speech_text = answers['GAST_ANSWER']
 
         # something went wrong
         else:
             speech_text = "Tut mir leid, aber diese Angehörigkeit gibt es nicht."
 
         # deactivate session before returning
-        self.deactivate_session(db_collection_answers)
+        self.deactivate_session(db_collection_session)
 
         handler_input.response_builder.speak(speech_text).ask(speech_text)
         return handler_input.response_builder.response
