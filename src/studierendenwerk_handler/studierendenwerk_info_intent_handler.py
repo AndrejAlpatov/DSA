90ec88fe409475f5f340188259c53958527ede07
Danny Staus <inf3719@hs-worms.de>
2021-11-27 18:20:39 +0100

    outsourced the answers to DB

--- src/studierendenwerk_handler/studierendenwerk_info_intent_handler.py
+++ src/studierendenwerk_handler/studierendenwerk_info_intent_handler.py
@@ -8,6 +8,7 @@ from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
+from src.data_bank_functions.output_of_all_collections import data_bank_access
 
 class StudierendenwerkInfoIntentHandler(AbstractRequestHandler):
     """Handler for StudierendenwerkInfoIntent."""
@@ -19,10 +20,18 @@ class StudierendenwerkInfoIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
 
-        speech_text = "Das Studierendenwerk Vorderpfalz ist eine rechtsfähige Anstalt des öffentlichen Rechts. " \
-                      "Ich kann dir Informationen zu den Themen und Aktivitäten oder anderen Standorten " \
-                      "von Mensen und Cafeterien mitteilen."
-        reprompt = "Sage zum Beispiel \"Was macht das Studierendenwerk Vorderpfalz?\""
+        # Get DB collections
+        list_with_collections = data_bank_access(['answers_stud_werk'])
+        db_collection_answers = list_with_collections[0]
+
+        # Get documents from collections
+        answers = db_collection_answers.find_one({})
+
+        # Select answer
+        speech_text = answers['INFO_STUD_WERK']
+
+        # Select reprompt
+        reprompt = answers['INFO_STUD_WERK_REPROMPT']
 
         handler_input.response_builder.speak(speech_text).ask(reprompt)
         return handler_input.response_builder.response
