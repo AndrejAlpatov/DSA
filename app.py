d2828220000d06c03c63843ee7de205aa6e0aaa2
Andrej Alpatov <inf3407@hs-worms.de>
2021-11-28 13:51:24 +0100

    code: add class OpeningHoursIntentHandler

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from src.kiosk_handler.namen_ausgeben_handler import NamenDerDBAusgebenHandler
 from src.kiosk_handler.additional_kiosk_questions_handler import IsThereQuestionsHandler, OwnCupInKioskHandler
 from src.kiosk_handler.kiosk_menu_intent_handler import KioskMenuWhatIntentHandler, KioskMenuIfIntentHandler
+from src.opening_hours_handler.opening_hours_handler import OpeningHoursIntentHandler
 
 
 
@@ -225,6 +226,7 @@ sb.add_request_handler(IsThereQuestionsHandler())
 sb.add_request_handler(OwnCupInKioskHandler())
 sb.add_request_handler(KioskMenuWhatIntentHandler())
 sb.add_request_handler(KioskMenuIfIntentHandler())
+sb.add_request_handler(OpeningHoursIntentHandler())
 
 skill_adapter = SkillAdapter(
     skill=sb.create(), skill_id=1, app=app)
