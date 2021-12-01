c9140171afe3b2ea8a299a1d2280fea7aba7cbaa
Marco Ohnesorg <marcoohnesorg@aol.com>
2021-12-01 08:07:49 +0100

    Update FTPManager.py

--- src/ftp/FTPManager.py
+++ src/ftp/FTPManager.py
@@ -3,7 +3,7 @@ import paramiko
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('ftp.mensaskill.de', port=22, username = 'u106517548-python',password ='!python?')
+ssh.connect()
 
 sftp = ssh.open_sftp()
 sftp.put('test.txt','upload.txt')
