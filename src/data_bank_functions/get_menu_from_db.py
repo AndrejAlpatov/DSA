5626870f834eb7388ba2e29851429a606bf60d82
Andrej Alpatov <inf3407@hs-worms.de>
2021-12-06 13:11:41 +0100

    code: add function get_menus_from_db()

--- src/data_bank_functions/get_menu_from_db.py
+++ src/data_bank_functions/get_menu_from_db.py
@@ -1,12 +1,21 @@
 from src.data_bank_functions.output_of_all_collections import data_bank_access
+import src.data_bank_functions.time_functions as time_func
 
 
-def get_menu_from_db(date):
-
-
+def get_menus_from_db(date_as_str):
+    week_number = time_func.week_number_for_date(date_as_str)
 
     # Get DB collection with menu for week "week_number"
     list_with_collections = data_bank_access([str(week_number)])
     db_collection_current_week_menu = list_with_collections[0]
 
-    ausgbe_1 = db_collection_current_week_menu.find({''})
+    # Get all documents from collection, where field "date" = date_as_str
+    documents_from_collection = db_collection_current_week_menu.find({'date': date_as_str})
+
+    list_with_menus = []  # list, to witch all arrays with menu will be append
+
+    # extract menu-arrays from documents and append to list
+    for document in documents_from_collection:
+        list_with_menus.append(document['menue'])
+
+    return list_with_menus
