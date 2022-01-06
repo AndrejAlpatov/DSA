79b80083c49fde534421de2c0e818f4b65ba57b5
Danny Staus <inf3719@hs-worms.de>
2022-01-06 21:16:22 +0100

    shortened imports

--- src/help_intent_handler/help_intent_handler.py
+++ src/help_intent_handler/help_intent_handler.py
@@ -2,8 +2,7 @@ from flask import Flask
 from pymongo import MongoClient
 from ask_sdk_core.skill_builder import SkillBuilder
 from flask_ask_sdk.skill_adapter import SkillAdapter
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model.ui import SimpleCard
